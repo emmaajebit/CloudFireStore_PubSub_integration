7c9d0a5cbd613c13cee100b5b5756527cd4317c7
Sami Islam <islam.samipkr@gmail.com>
2023-04-19 16:13:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def hello_firestore(event, context):
     print(f"Function triggered by change to: {resource_string}.")
     # now print out the entire event object
     print(str(event))
-
-    topic_name = 'projects/avian-amulet-378416/topics/func-1-demo'
+#projects/avian-amulet-378416/topics/func-1-demo
+    topic_name = 'func-1-demo'
     PROJECT_ID =  'avian-amulet-378416'
     message = str(event)
 
